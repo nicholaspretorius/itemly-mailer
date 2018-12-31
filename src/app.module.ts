e8f32c14f032e018f67fd84d58215cb85b9032be
Nicholas Pretorius <nicholaspretorius@gmail.com>
2018-12-31 16:44:45 +0200

    Added basic Nest.js Nodemailer module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailerController } from './mailer/mailer.controller';
+import { MailerModule } from '@nest-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot(),
+  ],
   controllers: [AppController, MailerController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
--- src/mailer/mailer.controller.ts
+++ src/mailer/mailer.controller.ts
@@ -1,13 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('mailer')
 export class MailerController {
 
+    constructor(@Inject('MailerProvider') private readonly mailerProvider) {}
+
     @MessagePattern({ cmd: 'register' })
     async register(payload: any): Promise<any> {
+
+        const response = await this.send(payload);
+
         // tslint:disable-next-line: no-console
-        console.log('Received!');
-        return `Register email sent: ` + payload;
+        console.log('Response: ', response);
+
+        if (response.accepted) {
+            return {
+                message: 'success',
+                data: payload,
+            };
+        } else {
+            return {
+                message: 'failed',
+                data: [],
+            };
+        }
+    }
+
+    private async send(payload): Promise<any> {
+        return await this.mailerProvider.sendMail({
+            to: payload.to,
+            from: 'noreply@itemly.com',
+            subject: payload.subject,
+            text: payload.message,
+            html: `<h3>${payload.message}</h3>`,
+        });
     }
 }
